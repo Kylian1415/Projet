dff9a8999e23fcd408f0e6a9e4afb599290f9756
Alexandre RICHARD-BONNET <E146198L@U-INF-J-F113-01.iut-nantes.univ-nantes.prive>
2016-03-24 09:13:00 +0100

    maj 3

--- Imports/Activities/ActivityCreator.py
+++ Imports/Activities/ActivityCreator.py
@@ -3,7 +3,7 @@ import sqlite3
 from Imports.Activities.ActivityLine import ActivityLine
 
 def insertActivity(activity):
-    conn = sqlite3.connect('installations.db')
+    conn = sqlite3.connect('myDB.db')
     c = conn.cursor()
     insertQuery = "INSERT INTO activities(id_activ, nom_activ) VALUES (?, ?)"
     c.execute(insertQuery, (activity.id_activ, activity.nom_activ))
